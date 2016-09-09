5b58d466c0d50b2b50e021aad1f226036416cbd0
ryym <ryym.64@gmail.com>
2016-09-09 23:39:29 +0900

    fixup! Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true
+    }
+  },
+
   "extends": "ryym/base"
 }
